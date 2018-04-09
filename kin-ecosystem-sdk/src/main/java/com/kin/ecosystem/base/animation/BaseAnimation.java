51e68af9940483646a0d45ae339c57949f90b1e9
OrenZak <orenzak24@gmail.com>
2018-04-09 10:24:55 +0300

    use the fork of base recycler adapter

--- kin-ecosystem-sdk/src/main/java/com/kin/ecosystem/base/animation/BaseAnimation.java
+++
@@ -1,8 +0,0 @@
-package com.kin.ecosystem.base.animation;
-
-import android.animation.Animator;
-import android.view.View;
-
-public interface BaseAnimation {
-    Animator[] getAnimators(View view);
-}
