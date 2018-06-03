c49ae81ec3efe1aba0b6657a031e4c8212f50d68
OrenZak <orenzak24@gmail.com>
2018-06-03 16:24:30 +0300

    remove uuid from SignInData

--- app/src/main/java/com/ecosystem/kin/app/App.java
+++ app/src/main/java/com/ecosystem/kin/app/App.java
@@ -5,8 +5,8 @@ import android.support.annotation.NonNull;
 import com.crashlytics.android.Crashlytics;
 import com.ecosystem.kin.app.model.SignInRepo;
 import com.kin.ecosystem.Kin;
+import com.kin.ecosystem.data.model.WhiteListData;
 import com.kin.ecosystem.exception.InitializeException;
-import com.kin.ecosystem.network.model.SignInData;
 import io.fabric.sdk.android.Fabric;
 
 
@@ -19,25 +19,30 @@ public class App extends Application {
 
         Fabric.with(this, new Crashlytics());
 
-        SignInData signInData;
-
         if (BuildConfig.IS_JWT_REGISTRATION) {
             /*
             * SignInData should be created with registration JWT {see https://jwt.io/} created securely by server side
-            * In the the this example 'SignInRepo.getJWTSignInData' receive an empty/null JWT String and the sample app generate the JWT locally.
+            * In the the this example 'SignInRepo.getJWT' receive an empty/null JWT String and the sample app generate the JWT locally.
             * DO NOT!!!! use this approach in your real app.
             * */
-            signInData = SignInRepo.getJWTSignInData(this, null);
+            String jwt = SignInRepo.getJWT(this);
+
+            try {
+                Kin.start(getApplicationContext(), jwt);
+            } catch (InitializeException e) {
+                e.printStackTrace();
+            }
         } else {
             //Use whitelist signing data for small scale testing
-            signInData = SignInRepo.getWhitelistSignInData(this, getAppId(), getApiKey());
+            WhiteListData whiteListData = SignInRepo.getWhitelistSignInData(this, getAppId(), getApiKey());
+            try {
+                Kin.start(getApplicationContext(), whiteListData);
+            } catch (InitializeException e) {
+                e.printStackTrace();
+            }
         }
 
-        try {
-            Kin.start(getApplicationContext(), signInData);
-        } catch (InitializeException e) {
-            e.printStackTrace();
-        }
+
     }
 
     @NonNull
--- app/src/main/java/com/ecosystem/kin/app/model/SignInRepo.java
+++ app/src/main/java/com/ecosystem/kin/app/model/SignInRepo.java
@@ -3,53 +3,22 @@ package com.ecosystem.kin.app.model;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.text.TextUtils;
 import com.ecosystem.kin.app.BuildConfig;
 import com.ecosystem.kin.app.JwtUtil;
-import com.kin.ecosystem.network.model.SignInData;
-import com.kin.ecosystem.network.model.SignInData.SignInTypeEnum;
+import com.kin.ecosystem.data.model.WhiteListData;
 import java.util.UUID;
 
 public class SignInRepo {
 
     private final static String USER_PREFERENCE_FILE_KEY = "USER_PREFERENCE_FILE_KEY";
     private final static String USER_UUID_KEY = "USER_UUID_KEY";
-    private static final String DEVICE_UUID_KEY = "DEVICE_UUID_KEY";
 
-    public static SignInData getWhitelistSignInData(Context context, @NonNull String appId, @NonNull String apiKey) {
-        SignInData signInData = createSignInDataWithDeviceID(context);
-        signInData.signInType(SignInTypeEnum.WHITELIST)
-            .appId(appId)
-            .userId(getUserId(context))
-            .apiKey(apiKey);
-        return signInData;
+    public static WhiteListData getWhitelistSignInData(Context context, @NonNull String appId, @NonNull String apiKey) {
+        return new WhiteListData(getUserId(context), appId, apiKey);
     }
 
-    public static SignInData getJWTSignInData(Context context, @Nullable String jwt) {
-        SignInData signInData = createSignInDataWithDeviceID(context);
-        signInData.signInType(SignInTypeEnum.JWT);
-
-        if (TextUtils.isEmpty(jwt)) {
-            signInData.jwt(JwtUtil.generateSignInExampleJWT(getAppId(), getUserId(context)));
-        } else {
-            signInData.jwt(jwt);
-        }
-        return signInData;
-    }
-
-    private static SignInData createSignInDataWithDeviceID(Context context) {
-        SignInData signInData = new SignInData();
-
-        SharedPreferences sharedPreferences = getSharedPreferences(context);
-
-        String deviceUUID = sharedPreferences.getString(DEVICE_UUID_KEY, null);
-        if (deviceUUID == null) {
-            deviceUUID = UUID.randomUUID().toString();
-            sharedPreferences.edit().putString(DEVICE_UUID_KEY, deviceUUID).apply();
-        }
-        signInData.deviceId(deviceUUID);
-        return signInData;
+    public static String getJWT(Context context) {
+        return JwtUtil.generateSignInExampleJWT(getAppId(), getUserId(context));
     }
 
     @NonNull
