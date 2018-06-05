a4dbc18ce64a88220c5af7e9ee35149cd88fbdab
OrenZak <orenzak24@gmail.com>
2018-06-05 10:31:27 +0300

    add purchase conflict test

--- app/src/main/java/com/ecosystem/kin/app/MainActivity.java
+++ app/src/main/java/com/ecosystem/kin/app/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         new NativeSpendOffer(String.valueOf(randomID))
             .title("Get Themes")
             .description("Personalize your chat")
-            .amount(1000)
+            .amount(100)
             .image("https://s3.amazonaws.com/kinmarketplace-assets/version1/kik_theme_offer+2.png");
 
     @Override
