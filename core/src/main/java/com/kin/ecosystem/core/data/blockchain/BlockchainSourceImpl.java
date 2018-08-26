49b3cab9b502c8c305ec209f6522d0cb2cbadfc8
OrenZak <orenzak24@gmail.com>
2018-08-26 16:26:23 +0300

    bi spend order completed fired twice

--- core/src/main/java/com/kin/ecosystem/core/data/blockchain/BlockchainSourceImpl.java
+++ core/src/main/java/com/kin/ecosystem/core/data/blockchain/BlockchainSourceImpl.java
@@ -6,18 +6,20 @@ import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
 import android.text.TextUtils;
 import com.kin.ecosystem.common.KinCallback;
-import com.kin.ecosystem.common.exception.BlockchainException;
-import com.kin.ecosystem.core.Log;
-import com.kin.ecosystem.core.bi.events.KinBalanceUpdated;
-import com.kin.ecosystem.core.Logger;
+import com.kin.ecosystem.common.KinCallbackAdapter;
 import com.kin.ecosystem.common.ObservableData;
 import com.kin.ecosystem.common.Observer;
-import com.kin.ecosystem.common.KinCallbackAdapter;
+import com.kin.ecosystem.common.exception.BlockchainException;
+import com.kin.ecosystem.common.model.Balance;
+import com.kin.ecosystem.core.Log;
+import com.kin.ecosystem.core.Logger;
 import com.kin.ecosystem.core.bi.EventLogger;
+import com.kin.ecosystem.core.bi.events.KinBalanceUpdated;
 import com.kin.ecosystem.core.bi.events.SpendTransactionBroadcastToBlockchainFailed;
 import com.kin.ecosystem.core.bi.events.SpendTransactionBroadcastToBlockchainSubmitted;
 import com.kin.ecosystem.core.bi.events.SpendTransactionBroadcastToBlockchainSucceeded;
-import com.kin.ecosystem.common.model.Balance;
+import com.kin.ecosystem.core.util.ErrorUtil;
+import com.kin.ecosystem.core.util.ExecutorsUtil.MainThreadExecutor;
 import java.math.BigDecimal;
 import kin.core.EventListener;
 import kin.core.KinAccount;
@@ -27,8 +29,6 @@ import kin.core.PaymentInfo;
 import kin.core.ResultCallback;
 import kin.core.TransactionId;
 import kin.core.exception.CreateAccountException;
-import com.kin.ecosystem.core.util.ErrorUtil;
-import com.kin.ecosystem.core.util.ExecutorsUtil.MainThreadExecutor;
 
 public class BlockchainSourceImpl implements BlockchainSource {
 
@@ -67,7 +67,8 @@ public class BlockchainSourceImpl implements BlockchainSource {
 	private static final int ORDER_ID_INDEX = 2;
 	private static final int MEMO_SPLIT_LENGTH = 3;
 
-	private BlockchainSourceImpl(@NonNull EventLogger eventLogger, @NonNull final KinClient kinClient, @NonNull BlockchainSource.Local local)
+	private BlockchainSourceImpl(@NonNull EventLogger eventLogger, @NonNull final KinClient kinClient,
+		@NonNull BlockchainSource.Local local)
 		throws BlockchainException {
 		this.eventLogger = eventLogger;
 		this.kinClient = kinClient;
@@ -76,7 +77,8 @@ public class BlockchainSourceImpl implements BlockchainSource {
 		initBalance();
 	}
 
-	public static void init(@NonNull EventLogger eventLogger, @NonNull final KinClient kinClient, @NonNull BlockchainSource.Local local)
+	public static void init(@NonNull EventLogger eventLogger, @NonNull final KinClient kinClient,
+		@NonNull BlockchainSource.Local local)
 		throws BlockchainException {
 		if (instance == null) {
 			synchronized (BlockchainSourceImpl.class) {
@@ -290,11 +292,11 @@ public class BlockchainSourceImpl implements BlockchainSource {
 			.addPaymentListener(new EventListener<PaymentInfo>() {
 				@Override
 				public void onEvent(PaymentInfo data) {
-					String orderID = extractOrderId(data.memo());
+					final String orderID = extractOrderId(data.memo());
 					Logger.log(new Log().withTag(TAG).put("startPaymentListener onEvent: the orderId", orderID)
 						.put("with memo", data.memo()));
 					if (orderID != null) {
-						completedPayment.postValue(new Payment(orderID, data.hash().id(), data.amount()));
+						completedPayment.postValue(toPaymentObj(orderID, data));
 						Logger.log(new Log().withTag(TAG).put("completedPayment order id", orderID));
 					}
 					// UpdateBalance if there is no balance sse open connection.
@@ -309,6 +311,11 @@ public class BlockchainSourceImpl implements BlockchainSource {
 					}
 				}
 			});
+	}
+
+	private Payment toPaymentObj(final String orderID, PaymentInfo data) {
+		boolean isEarn = data.destinationPublicKey().equals(getPublicAddress());
+		return new Payment(orderID, data.hash().id(), data.amount(), isEarn ? Payment.EARN : Payment.SPEND);
 	}
 
 	@Override
--- core/src/main/java/com/kin/ecosystem/core/data/blockchain/Payment.java
+++ core/src/main/java/com/kin/ecosystem/core/data/blockchain/Payment.java
@@ -1,6 +1,9 @@
 package com.kin.ecosystem.core.data.blockchain;
 
+import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.math.BigDecimal;
 
 /**
@@ -9,69 +12,88 @@ import java.math.BigDecimal;
  */
 public class Payment {
 
-    /**
-     * Order id that the transaction is related to.
-     */
-    private String orderID;
+	public static final int UNKNOWN = 0x00000000;
+	public static final int EARN = 0x00000001;
+	public static final int SPEND = 0x00000002;
 
-    /**
-     * The transaction id on the blockchain, could be null if the transaction failed.
-     */
-    private @Nullable String transactionID;
+	@IntDef({EARN, SPEND})
+	@Retention(RetentionPolicy.SOURCE)
+	public @interface Type {
 
-    /**
-     * The payment amount was sent / received, could be null id transaction failed.
-     */
-    private @Nullable BigDecimal amount;
+	}
 
-    /**
-     * Determine if the transaction succeeded or not.
-     */
-    private boolean isSucceed;
+	/**
+	 * Order id that the transaction is related to.
+	 */
+	private String orderID;
 
-    /**
-     * Exception from kin-core:
-     */
-    private Exception exception;
+	/**
+	 * The transaction id on the blockchain, could be null if the transaction failed.
+	 */
+	private @Nullable
+	String transactionID;
 
-    public Payment(String orderID, @Nullable String transactionID, @Nullable BigDecimal amount) {
-        this.orderID = orderID;
-        this.transactionID = transactionID;
-        this.amount = amount;
-        this.isSucceed = true;
-    }
+	/**
+	 * The payment amount was sent / received, could be null id transaction failed.
+	 */
+	private @Nullable
+	BigDecimal amount;
 
-    public Payment(String orderID, boolean isSucceed, Exception error) {
-        this.orderID = orderID;
-        this.transactionID = null;
-        this.amount = null;
-        this.isSucceed = isSucceed;
-        this.exception = error;
-    }
+	/**
+	 * Determine if the transaction succeeded or not.
+	 */
+	private boolean isSucceed;
 
-    public String getOrderID() {
-        return orderID;
-    }
+	/**
+	 * Exception from kin-core:
+	 */
+	private Exception exception;
 
-    @Nullable
-    public String getTransactionID() {
-        return transactionID;
-    }
+	/**
+	 * The {@link Type} of payment: EARN, SPEND or UNKNOWN if the payment failed.
+	 */
+	private @Type
+	int type = UNKNOWN;
 
-    @Nullable
-    public BigDecimal getAmount() {
-        return amount;
-    }
+	public Payment(String orderID, @Nullable String transactionID, @Nullable BigDecimal amount, @Type int type) {
+		this.orderID = orderID;
+		this.transactionID = transactionID;
+		this.amount = amount;
+		this.isSucceed = true;
+		this.type = type;
+	}
 
-    public boolean isSucceed() {
-        return isSucceed;
-    }
+	public Payment(String orderID, boolean isSucceed, Exception error) {
+		this.orderID = orderID;
+		this.transactionID = null;
+		this.amount = null;
+		this.isSucceed = isSucceed;
+		this.exception = error;
+	}
 
-    public Exception getException() {
-        return exception;
-    }
+	public String getOrderID() {
+		return orderID;
+	}
 
-	public boolean isEarn() {
-		return amount != null && amount.compareTo(BigDecimal.ZERO) == 1;
+	@Nullable
+	public String getTransactionID() {
+		return transactionID;
+	}
+
+	@Nullable
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public boolean isSucceed() {
+		return isSucceed;
+	}
+
+	public Exception getException() {
+		return exception;
+	}
+
+	public int getType() {
+		return type;
 	}
 }
