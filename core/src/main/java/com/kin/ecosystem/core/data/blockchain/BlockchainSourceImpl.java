637faa8b281e87dc489edc330968ba03c0132ab1
Yohay Barsky <YohayBar@users.noreply.github.com>
2018-10-18 14:24:43 +0300

    Revert "add backup module"

--- core/src/main/java/com/kin/ecosystem/core/data/blockchain/BlockchainSourceImpl.java
+++ core/src/main/java/com/kin/ecosystem/core/data/blockchain/BlockchainSourceImpl.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
 import android.text.TextUtils;
-import com.kin.ecosystem.backup.KeyStoreProvider;
 import com.kin.ecosystem.common.KinCallback;
 import com.kin.ecosystem.common.KinCallbackAdapter;
 import com.kin.ecosystem.common.ObservableData;
@@ -354,11 +353,6 @@ public class BlockchainSourceImpl implements BlockchainSource {
 				});
 			}
 		}).start();
-	}
-
-	@Override
-	public KeyStoreProvider getKeyStoreProvider() {
-		return new KeyStoreProviderImpl();
 	}
 
 	private void decrementPaymentCount() {
