4bb064d26680522eda65082d4046d43ef254ba7b
Yossi Rizgan <yosriz@gmail.com>
2018-10-23 22:08:42 +0300

    divide RestoreBackupEvents to events/backup, fix build error

--- recovery/src/main/java/com/kin/ecosystem/recovery/BackupEvents.java
+++ recovery/src/main/java/com/kin/ecosystem/recovery/BackupEvents.java
@@ -1,26 +1,32 @@
 package com.kin.ecosystem.recovery;
 
 public interface BackupEvents {
-    void onBackupWelcomePageViewed();
-    void onBackupWelcomePageBackButtonTapped();
-    void onBackupStartButtonTapped();
-    void onBackupCreatePasswordPageViewed();
-    void onBackupCreatePasswordBackButtonTapped();
-    void onBackupCreatePasswordNextButtonTapped();
-    void onBackupQrCodePageViewed();
-    void onBackupQrCodeBackButtonTapped();
-    void onBackupQrCodeSendButtonTapped();
-    void onBackupQrCodeMyQrCodeButtonTapped();
-    void onBackupCompletedPageViewed();
-    void onBackupPopupPageViewed();
-    void onBackupPopupButtonTapped();
-    void onBackupPopupLaterButtonTapped();
-    void onRestoreUploadQrCodePageViewed();
-    void onRestoreUploadQrCodeBackButtonTapped();
-    void onRestoreUploadQrCodeButtonTapped();
-    void onRestoreAreYouSureOkButtonTapped();
-    void onRestoreAreYouSureCancelButtonTapped();
-    void onRestorePasswordEntryPageViewed();
-    void onRestorePasswordEntryBackButtonTapped();
-    void onRestorePasswordDoneButtonTapped();
+
+	void onBackupWelcomePageViewed();
+
+	void onBackupWelcomePageBackButtonTapped();
+
+	void onBackupStartButtonTapped();
+
+	void onBackupCreatePasswordPageViewed();
+
+	void onBackupCreatePasswordBackButtonTapped();
+
+	void onBackupCreatePasswordNextButtonTapped();
+
+	void onBackupQrCodePageViewed();
+
+	void onBackupQrCodeBackButtonTapped();
+
+	void onBackupQrCodeSendButtonTapped();
+
+	void onBackupQrCodeMyQrCodeButtonTapped();
+
+	void onBackupCompletedPageViewed();
+
+	void onBackupPopupPageViewed();
+
+	void onBackupPopupButtonTapped();
+
+	void onBackupPopupLaterButtonTapped();
 }
