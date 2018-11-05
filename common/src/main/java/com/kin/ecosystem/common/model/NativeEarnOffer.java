b228d42c57d2c44f8b385eac1ebd3d6761980a9e
YohayBar <yohay@kik.com>
2018-11-05 16:05:08 +0200

    minor

--- common/src/main/java/com/kin/ecosystem/common/model/NativeEarnOffer.java
+++ common/src/main/java/com/kin/ecosystem/common/model/NativeEarnOffer.java
@@ -1,8 +1,10 @@
 package com.kin.ecosystem.common.model;
 
+import android.support.annotation.NonNull;
+
 public class NativeEarnOffer extends NativeOffer {
 
-	public NativeEarnOffer(String id) {
+	public NativeEarnOffer(@NonNull String id) {
 		super(id);
 	}
 
--- common/src/main/java/com/kin/ecosystem/common/model/NativeSpendOffer.java
+++ common/src/main/java/com/kin/ecosystem/common/model/NativeSpendOffer.java
@@ -1,8 +1,10 @@
 package com.kin.ecosystem.common.model;
 
+import android.support.annotation.NonNull;
+
 public class NativeSpendOffer extends NativeOffer {
 
-	public NativeSpendOffer(String id) {
+	public NativeSpendOffer(@NonNull String id) {
 		super(id);
 	}
 
