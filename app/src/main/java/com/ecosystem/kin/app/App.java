b92c3b7c4e5a479f27492d58d1192399c00b907d
OrenZak <orenzak24@gmail.com>
2018-12-04 12:04:33 +0200

    leakcanary in debug

--- app/src/main/java/com/ecosystem/kin/app/App.java
+++ app/src/main/java/com/ecosystem/kin/app/App.java
@@ -1,7 +1,6 @@
 package com.ecosystem.kin.app;
 
 import android.app.Application;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import com.crashlytics.android.Crashlytics;
 import com.ecosystem.kin.app.model.SignInRepo;
@@ -18,56 +17,53 @@ import io.fabric.sdk.android.Fabric;
 public class App extends Application {
 
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        if(BuildConfig.DEBUG) {
-            if (LeakCanary.isInAnalyzerProcess(this)) {
-                // This process is dedicated to LeakCanary for heap analysis.
-                // You should not init your app in this process.
-                return;
-            }
-            LeakCanary.install(this);
-        }
-        Fabric.with(this, new Crashlytics());
-
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		if (LeakCanary.isInAnalyzerProcess(this)) {
+			// This process is dedicated to LeakCanary for heap analysis.
+			// You should not init your app in this process.
+			return;
+		}
+		LeakCanary.install(this);
+		Fabric.with(this, new Crashlytics());
 
 		KinEnvironment environment = Environment.getBeta();
 
-        if (BuildConfig.IS_JWT_REGISTRATION) {
-            /**
-            * SignInData should be created with registration JWT {see https://jwt.io/} created securely by server side
-            * In the the this example {@link SignInRepo#getJWT} generate the JWT locally.
-            * DO NOT!!!! use this approach in your real app.
-            * */
-            String jwt = SignInRepo.getJWT(this);
+		if (BuildConfig.IS_JWT_REGISTRATION) {
+			/**
+			 * SignInData should be created with registration JWT {see https://jwt.io/} created securely by server side
+			 * In the the this example {@link SignInRepo#getJWT} generate the JWT locally.
+			 * DO NOT!!!! use this approach in your real app.
+			 * */
+			String jwt = SignInRepo.getJWT(this);
 
-            try {
-                Kin.start(getApplicationContext(), jwt, environment);
-            } catch (ClientException | BlockchainException e) {
-                e.printStackTrace();
-            }
-        } else {
-            /** Use {@link WhitelistData} for small scale testing */
-            WhitelistData whitelistData = SignInRepo.getWhitelistSignInData(this, getAppId(), getApiKey());
-            try {
-                Kin.start(getApplicationContext(), whitelistData, environment);
-            } catch (ClientException | BlockchainException e) {
-                e.printStackTrace();
-            }
-        }
+			try {
+				Kin.start(getApplicationContext(), jwt, environment);
+			} catch (ClientException | BlockchainException e) {
+				e.printStackTrace();
+			}
+		} else {
+			/** Use {@link WhitelistData} for small scale testing */
+			WhitelistData whitelistData = SignInRepo.getWhitelistSignInData(this, getAppId(), getApiKey());
+			try {
+				Kin.start(getApplicationContext(), whitelistData, environment);
+			} catch (ClientException | BlockchainException e) {
+				e.printStackTrace();
+			}
+		}
 
-        Kin.enableLogs(true);
-    }
+		Kin.enableLogs(true);
+	}
 
-    @NonNull
-    private static String getAppId() {
-        return BuildConfig.SAMPLE_APP_ID;
-    }
+	@NonNull
+	private static String getAppId() {
+		return BuildConfig.SAMPLE_APP_ID;
+	}
 
-    @NonNull
-    private static String getApiKey() {
-        return BuildConfig.SAMPLE_API_KEY;
-    }
+	@NonNull
+	private static String getApiKey() {
+		return BuildConfig.SAMPLE_API_KEY;
+	}
 
 }
