0209ccc2f4b6c82b0d3f963c9ec56e1360c9b251
OrenZak <orenzak24@gmail.com>
2018-12-12 16:40:31 +0200

    fix offers item size

--- sdk/src/main/java/com/kin/ecosystem/marketplace/view/EarnRecyclerAdapter.java
+++ sdk/src/main/java/com/kin/ecosystem/marketplace/view/EarnRecyclerAdapter.java
@@ -1,10 +1,29 @@
 package com.kin.ecosystem.marketplace.view;
 
+import static com.kin.ecosystem.core.util.DeviceUtils.DensityDpi.XXHDPI;
+
 import com.kin.ecosystem.R;
+import com.kin.ecosystem.core.util.DeviceUtils;
 
 class EarnRecyclerAdapter extends OfferRecyclerAdapter {
+
+    private static final float IMAGE_WIDTH_RATIO = 0.388f;
+    private static final float IMAGE_HEIGHT_RATIO = 0.714f;
+
+	private static final float IMAGE_WIDTH_XX_RES_RATIO = 0.333f;
+	private static final float IMAGE_HEIGHT_XX_RES_RATIO = 0.664f;
 
     EarnRecyclerAdapter() {
         super(R.layout.kinecosystem_earn_recycler_item);
     }
+
+	@Override
+	protected float getImageWidthToScreenRatio() {
+		return DeviceUtils.isDensity(XXHDPI) ? IMAGE_WIDTH_XX_RES_RATIO : IMAGE_WIDTH_RATIO;
+	}
+
+	@Override
+	protected float getImageHeightRatio() {
+		return DeviceUtils.isDensity(XXHDPI) ? IMAGE_HEIGHT_XX_RES_RATIO : IMAGE_HEIGHT_RATIO;
+	}
 }
