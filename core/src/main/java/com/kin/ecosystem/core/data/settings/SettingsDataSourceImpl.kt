dff041a731d19a9d384e5375d219d2775e57dba2
Shay Baz <shay@rounds.com>
2019-06-23 18:08:20 +0300

    KINNO-625-settings_entry

--- core/src/main/java/com/kin/ecosystem/core/data/settings/SettingsDataSourceImpl.kt
+++ core/src/main/java/com/kin/ecosystem/core/data/settings/SettingsDataSourceImpl.kt
@@ -1,7 +1,6 @@
 package com.kin.ecosystem.core.data.settings
 
 class SettingsDataSourceImpl(private val local: SettingsDataSource.Local) : SettingsDataSource {
-
     override fun setIsBackedUp(publicAddress: String, isBackedUp: Boolean) {
         local.setIsBackedUp(publicAddress, isBackedUp)
     }
@@ -11,10 +10,18 @@ class SettingsDataSourceImpl(private val local: SettingsDataSource.Local) : Sett
     }
 
     override fun setSawOnboarding(kinUserId: String) {
-        local.setSawOnboarding(kinUserId)
+        local.setSeenOnboarding(kinUserId)
     }
 
-    override fun isSawOnboarding(kinUserId: String): Boolean {
-        return local.isSawOnboarding(kinUserId)
+    override fun hasSeenOnboarding(kinUserId: String): Boolean {
+        return local.hasSeenOnboarding(kinUserId)
+    }
+
+    override fun hasSeenTransfer(kinUserId: String): Boolean {
+        return local.hasSeenTransfer(kinUserId)
+    }
+
+    override fun setSeenTransfer(kinUserId: String) {
+        local.setSeenTransfer(kinUserId)
     }
 }
