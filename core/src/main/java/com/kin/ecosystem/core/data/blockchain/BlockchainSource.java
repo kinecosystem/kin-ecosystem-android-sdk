1e3ab8ded4a4b74f6393449fe6f28dc7b021fb82
Shay Baz <shay@rounds.com>
2019-07-07 18:46:53 +0300

    KINNO-611_send_kin_service_work_with_servers

--- core/src/main/java/com/kin/ecosystem/core/data/blockchain/BlockchainSource.java
+++ core/src/main/java/com/kin/ecosystem/core/data/blockchain/BlockchainSource.java
@@ -11,7 +11,6 @@ import com.kin.ecosystem.common.model.Balance;
 import com.kin.ecosystem.core.network.ApiException;
 import com.kin.ecosystem.core.network.model.MigrationInfo;
 import com.kin.ecosystem.recovery.KeyStoreProvider;
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
