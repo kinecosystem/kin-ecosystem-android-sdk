63715510c5da736bf9ea4f9b6eee75f931c5b601
Shay Baz <shay@rounds.com>
2019-07-07 16:09:45 +0300

    Shay/feature_send_rebased2

--- sdk/src/main/java/com/kin/ecosystem/transfer/view/AccountInfoActivity.java
+++ sdk/src/main/java/com/kin/ecosystem/transfer/view/AccountInfoActivity.java
@@ -1,10 +1,13 @@
 package com.kin.ecosystem.transfer.view;
 
+import com.kin.ecosystem.Kin;
 import com.kin.ecosystem.R;
 import com.kin.ecosystem.common.exception.BlockchainException;
+import com.kin.ecosystem.common.exception.ClientException;
 import com.kin.ecosystem.core.Log;
 import com.kin.ecosystem.core.Logger;
 import com.kin.ecosystem.core.data.auth.AuthRepository;
+import com.kin.ecosystem.core.data.blockchain.BlockchainSource;
 import com.kin.ecosystem.core.data.blockchain.BlockchainSourceImpl;
 
 import org.kinecosystem.transfer.receiver.manager.AccountInfoException;
@@ -17,13 +20,24 @@ public class AccountInfoActivity extends AccountInfoActivityBase {
 
     @Override
     public String getData() throws AccountInfoException {
+        android.util.Log.d("####", "AccountInfoActivity   getData 1");
+
         try {
+            Kin.initialize(getApplicationContext(), null);
             final AuthRepository authRepository = AuthRepository.getInstance();
+            final BlockchainSource blockchainSource = BlockchainSourceImpl.getInstance();
+            android.util.Log.d("####", " getData authRepository   authRepository " + authRepository);
+            android.util.Log.d("####", " getData blockchainSource   blockchainSource " + authRepository);
+
             if (authRepository != null && !authRepository.isCurrentAuthTokenExpired()
-                    && BlockchainSourceImpl.getInstance() != null) {
-                return BlockchainSourceImpl.getInstance().getPublicAddress();
+                    && blockchainSource != null) {
+                return blockchainSource.getPublicAddress();// tPublicAddress();
             }
         } catch (BlockchainException e) {
+            android.util.Log.d("####", "error1 getData ##### e " + e.getMessage());
+            throwExceptionOnDataError();
+        } catch (ClientException e) {
+            android.util.Log.d("####", "error2 getData ##### e " + e.getMessage());
             throwExceptionOnDataError();
         }
         return null;
@@ -37,6 +51,8 @@ public class AccountInfoActivity extends AccountInfoActivityBase {
     @Override
     public void updateTransactionInfo(String senderAppId, String senderAppName, String receiverAppId, String memo) {
         super.updateTransactionInfo(senderAppId, senderAppName, receiverAppId, memo);
+        android.util.Log.d("####", "updateTransactionInfo ##### AccountInfoActivity Validate memo " + memo + " on blockChain and if valid add to transaction history sender App Name " + senderAppName + " sender app id: " + senderAppId + " receiver ppp Id: " + receiverAppId);
+
         //server needs to check transaction got with that memo on the blockchain
         //if found add senderAppName to transaction history
         Logger.log(new Log().withTag(TAG).put(TAG, "AccountInfoActivity Validate memo " + memo + " on blockChain and if valid add to transaction history sender App Name " + senderAppName + " sender app id: " + senderAppId + " receiver ppp Id: " + receiverAppId));
